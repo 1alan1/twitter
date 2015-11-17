b03f1019c8bcdf9cd7aa60071fbfe097a18c37e5
Dagar <dagar90@gmail.com>
2015-11-17 15:16:38 +0000

    Chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+ 
  root "static_pages#home"
  match "/about", to: "static_pages#about", via: :get
  match "/help", to: "static_pages#help", via: :get
  get "/signup", to: "users#new"
+ resources :users
+ 
 end
