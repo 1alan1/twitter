cf65eddccaa749f8bb83ed498e1979cef73cd749
Dagar <dagar90@gmail.com>
2015-11-26 13:09:06 +0000

    Chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
  match "/help", to: "static_pages#help", via: :get
  get "/signup", to: "users#new"
  get "/signin", to: "sessions#new"
+ match "/signout", to: "sessions#destroy", via: :delete
  
  resources :users
  resources :sessions, only: [:new, :create, :destroy]
